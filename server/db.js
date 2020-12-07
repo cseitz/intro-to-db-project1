414f4a3bdfcb1a13907f78e36c72d41d53fb8edf
cseitz5 <cseitz5@kent.edu>
2020-12-07 00:29:34 -0500

    a

--- server/db.js
+++ server/db.js
@@ -58,6 +58,7 @@ let db = {
 
 global.db = db;
 
+
 /*connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
   if (error) throw error;
   console.log('The solution is: ', results[0].solution);
